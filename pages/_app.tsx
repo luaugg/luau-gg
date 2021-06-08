bf06bad1d34098acc00030f07d77054baabb150b
Sam Pritchard <sampritchard0801@gmail.com>
2021-06-08 04:10:29 +0100

    no need for custom app component, add index.tsx

--- pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import { AppProps } from 'next/app'
-
-function Portfolio({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
-}
-
-export default Portfolio
---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+function Portfolio() {
+    return (
+        <div>
+            <Head>
+                <title>sam's portfolio</title>
+                <link rel="shortcut icon" href="./favicon.ico" />
+            </Head>
+        </div>
+    )
+}
+
+export default Portfolio
