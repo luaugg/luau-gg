64a510709743efd9959d83d9c95c1e89de9c7bf8
Sam Pritchard <sampritchard0801@gmail.com>
2021-06-11 08:14:23 +0100

    Add logos to header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,12 +9,22 @@ export default function DefaultApp({ Component, pageProps }: AppProps) {
                 <link rel="shortcut icon" href="./favicon.ico" />
             </Head>
 
-            <div className="flex space-x-8 pl-5 h-14 w-screen bg-white shadow-md">
+            <div className="flex space-x-8 pl-5 pr-5 h-14 w-screen bg-white shadow-md">
                 <img className="flex-initial my-auto rounded-full w-10 h-10" src="avatar.jpg" />
                 
-                <a className="relative flex-inital my-auto font-sans font-semibold text-gray-600 hover:text-red-500" href="/">About Me</a>
+                <a className="flex-inital my-auto font-sans font-semibold text-gray-600 hover:text-red-500" href="/">About Me</a>
 
-                <a className="relative flex-inital my-auto font-sans font-semibold text-gray-600 hover:text-red-500" href="/projects">Projects</a>
+                <a className="flex-inital my-auto font-sans font-semibold text-gray-600 hover:text-red-500" href="/projects">Projects</a>
+
+                <div className="flex-initial flex-grow" />
+
+                <a className="flex-initial my-auto" href="https://discord.gg/UbXkbrXxT7">
+                    <img className="w-9 h-9" src="discord.png" />
+                </a>
+
+                <a className="flex-initial my-auto" href="https://twitter.com/luauOW">
+                    <img className="w-8 h-7" src="twitter.png" />
+                </a>
             </div>
 
             <Component {...pageProps} />
