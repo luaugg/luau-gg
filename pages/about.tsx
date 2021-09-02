597e8c223bf0031bee6e5a4255768c4513c31f09
Sam Pritchard <sampritchard0801@gmail.com>
2021-09-02 12:35:47 +0100

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,7 +5,7 @@ export default function Portfolio() {
         <div className="flex w-screen h-screen">
             <div className="flex flex-col flex-initial flex-wrap mt-10 mx-auto w-1/3 h-auto xl:h-1/2 rounded-lg shadow bg-white space-y-5">
                 <div className="font-sans font-extralight sm:text-4xl text-xl text-gray-600 mx-auto mt-10">Hi, I'm Sam.</div>
-                <p className="font-sans font-extralight md:text-sm text-xs text-center text-gray-600 mx-auto w-4/5 h-1/2">
+                <p className="font-sans font-extralight font-weight-500 md:text-sm text-xs text-center text-gray-600 mx-auto w-4/5 h-1/2">
                     Nice to meet you!
                     <br />
                     I am a backend <a className="text-red-500 font-medium" href="https://github.com/SamOphis">software developer </a>  
