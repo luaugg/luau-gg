cc04602a4773c4e17afe388435bfb8668b9f6a64
Sam Pritchard <sampritchard0801@gmail.com>
2021-10-23 21:42:21 +0100

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -12,9 +12,9 @@ export default function Portfolio() {
                     ― that's why this site is so simple 😅 ― and also a lover of anything related to space 🚀 and flowers 🌺.
                     <br /><br />
 
-                    I play quite a bit of Overwatch as a DPS player, primarily focusing on Tracer, Sombra, Echo, Hanzo, Pharah and Ashe. 
-                    I manage community stuff for <a className="text-red-500 font-medium" href="https://powernappers.com">Power Nappers </a>
-                    as well as helping manage <a className="text-red-500 font-medium" href="https://twitter.com/PowerBlossomsOW">Power Blossoms</a> 📋.
+                    I currently play Flex DPS for a team called <a className="text-red-500 font-medium" href="https://twitter.com/PowerDragonsGa1">Power Dragons </a>.
+                    I also manage the <a className="text-red-500 font-medium" href="https://powernappers.com">Power Nappers </a> community as well as one of its teams:
+                    <a className="text-red-500 font-medium" href="https://twitter.com/PowerBlossomsOW">Power Blossoms</a> 📋.
                     <br /><br />
 
                     I'm 18 years old 🎂, use he/they pronouns 🏳️‍🌈 and also stream semi-regularly over 
